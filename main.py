fd57bbde3ae23bff796a0757200e34b98d0aff3e
lineageos357-star <lineageos357@gmail.com>
2026-04-22 18:53:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def is_youtube_url(url: str) -> bool:
 def get_base_ydl_opts():
     opts = {
         "quiet": True,
-        "extractor_args": {"youtube": {"player_client": ["web"], "skip": ["dash", "hls"]}},
+        "extractor_args": {"youtube": {"player_client": ["tv_embedded"]}},
         "http_headers": {
             "User-Agent": (
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
